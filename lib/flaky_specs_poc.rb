4674d3c7dec21fc5b1d6ade3e06d057cf010ac90
Bernat Rafales <bernat@bloomandwild.com>
2022-08-03 15:34:51 +0200

    Initial commit

---
+++ lib/flaky_specs_poc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "flaky_specs_poc/version"
+
+module FlakySpecsPoc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/flaky_specs_poc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FlakySpecsPoc
+  VERSION = "0.1.0"
+end
